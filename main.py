d4ec418eac81a6cde01265c6318294c28a7d87e6
soren5 <pedrofilipecarvalho@mac.com>
2019-03-26 20:38:41 +0000

    added comments

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 weather_dataframe_raw = pd.read_csv('weatherAUS.csv')
+
+#Remove features that have more than 20% of missing values
 n_weather_dataframe = weather_dataframe_raw.dropna(1, thresh=len(weather_dataframe_raw.index) * 0.2)
 print(n_weather_dataframe)
+
+#Remove examples that have any missing values
 n_weather_dataframe = weather_dataframe_raw.dropna(0, how='any')
 print(n_weather_dataframe)
