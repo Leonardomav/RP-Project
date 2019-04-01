9778cffc618950cfda6e099e16d6ec49398e5e37
Leonardomav <leomavieira97@gmail.com>
2019-04-01 16:04:40 +0100

    Basic Data information extraction

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import numpy
 from pandas import read_csv, set_option
 from pandas.plotting import scatter_matrix
 from sklearn.preprocessing import MinMaxScaler
+import data_info
 
+# Load data set
 filename = 'weatherAUS.csv'
 data_raw = read_csv(filename)
 
@@ -13,49 +15,18 @@ data = data_raw.dropna(1, thresh=len(data_raw.index) * 0.8)
 # Remove examples that have any missing values
 data = data.dropna(0, how='any')
 
-print(len(data.index))
-print(data.columns)
+# Print general info about the data
+# data_info.print_general_information(data)
 
+# Describe a Single Columns
+# data_info.describe_column(data, 'Location')
 
-peek = data.head(20)  # le as primeiras 20 linhas
-types = data.dtypes
-set_option('display.width', 100)
-set_option('precision', 3)
-description = data.describe()
-class_counts = data.groupby('RainTomorrow').size()
-correlation = data.corr(method='pearson')
-skew = data.skew()
-data.hist()
-data.plot(kind='density', subplots=True, layout=(4, 4), sharex=False)
-data.plot(kind='box', subplots=True, layout=(4, 4), sharex=False, sharey=False)
-scatter_matrix(data)
+# Print correlation between all features or between two if specified
+# data_info.correlation_info(data, 'pearson', "MinTemp", "MaxTemp")
 
-# correlation matrix
-fig = pyplot.figure()
-ax = fig.add_subplot(111)
-cax = ax.matshow(correlation, vmin=-1, vmax=1)
-fig.colorbar(cax)
-ticks = numpy.arange(0, 20, 1)
-ax.set_xticks(ticks)
-ax.set_yticks(ticks)
-ax.set_xticklabels(data.head())
-ax.set_yticklabels(data.head())
+# Print data swew
+# print(data.skew())
 
-# rescale data
-# array = data.values
-# separate array into input and output components
-# X = array[:, 0:8]
-# Y = array[:, 8]
-# scaler = MinMaxScaler(feature_range=(0,1))
-# rescaledX = scaler.fit(X)
-# summarize transformed data
-# numpy.set_printoptions(precision=3)
-
-pyplot.show()
-print(peek)
-print(data.shape)
-print(types)
-print(class_counts)
-print(description)
-print(correlation)
-print(skew)
+# Plot the features histogram
+# data.hist()
+# pyplot.show()
