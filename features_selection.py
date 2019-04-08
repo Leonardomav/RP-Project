97c40f69e7c65db6ac50a2a36bab08a02e523949
Leonardomav <leomavieira97@gmail.com>
2019-04-08 12:19:27 +0100

    kkw overflow

--- features_selection.py
+++ features_selection.py
@@ -1,7 +1,11 @@
 import scipy
+import numpy as np
 
 def kruskal_wallis(data):
     rank=[]
-    setX = data.loc[:, data.columns != "RainTomorrow"]
-    setY = data["RainTomorrow"].tolist()
-    scipy.stats.kruskal(setX,setY)
+    rank = np.array(rank, dtype=np.float64)
+    for i in range(data.shape[1]-3):
+        setX = data[data.columns[i+2]].tolist()
+        setY = data["RainTomorrow"].tolist()
+        rank.append(rank, scipy.stats.kruskal(setX,setY))
+    print(rank)
--- main.py
+++ main.py
@@ -17,6 +17,8 @@ data = data_raw.dropna(1, thresh=len(data_raw.index) * 0.8)
 # Remove examples that have any missing values
 data = data.dropna(0, how='any')
 
+
+
 #change Yes and No to 1 and 0
 data['RainTomorrow'] = data['RainTomorrow'].map({'Yes': 1, 'No': 0})
 
@@ -27,7 +29,7 @@ data['RainTomorrow'] = data['RainTomorrow'].map({'Yes': 1, 'No': 0})
 # data_info.describe_column(data, 'Location')
 
 # Print correlation between all features or between two if specified
-#data_info.correlation_info(data, 'pearson')
+data_info.correlation_info(data, 'pearson')
 
 # Print data skew
 # print(data.skew())
