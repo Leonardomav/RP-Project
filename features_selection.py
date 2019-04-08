502021078a6393d734d47c6f3db8a898c167810c
Leonardomav <leomavieira97@gmail.com>
2019-04-08 17:31:53 +0100

    All categorical now

--- features_selection.py
+++ features_selection.py
@@ -3,9 +3,8 @@ import numpy as np
 
 def kruskal_wallis(data):
     rank=[]
-    rank = np.array(rank, dtype=np.float64)
     for i in range(data.shape[1]-3):
         setX = data[data.columns[i+2]].tolist()
         setY = data["RainTomorrow"].tolist()
-        rank.append(rank, scipy.stats.kruskal(setX,setY))
+        rank.append(scipy.stats.kruskal(setX,setY))
     print(rank)
--- main.py
+++ main.py
@@ -17,19 +17,33 @@ data = data_raw.dropna(1, thresh=len(data_raw.index) * 0.8)
 # Remove examples that have any missing values
 data = data.dropna(0, how='any')
 
-
-
 #change Yes and No to 1 and 0
-data['RainTomorrow'] = data['RainTomorrow'].map({'Yes': 1, 'No': 0})
+data['RainTomorrow'] = data['RainTomorrow'].map({'Yes': 1, 'No': 0}).astype('category')
+data['RainToday'] = data['RainToday'].map({'Yes': 1, 'No': 0}).astype('category')
+
+
+labels = data['WindGustDir'].astype('category').cat.categories.tolist()
+col = ['WindGustDir', 'WindDir9am', 'WindDir3pm']
+
+for c in col:
+    replace_map = {c : {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
+    data.replace(replace_map, inplace=True)
+    data[c].astype('category')
+
+labels = data['Location'].astype('category').cat.categories.tolist()
+replace_map = {'Location': {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
+data.replace(replace_map, inplace=True)
+data['Location'].astype('category')
+
 
 # Print general info about the data
 #data_info.print_general_information(data)
 
 # Describe a Single Columns
-# data_info.describe_column(data, 'Location')
+# data_info.describe_column(data)
 
 # Print correlation between all features or between two if specified
-data_info.correlation_info(data, 'pearson')
+#data_info.correlation_info(data, 'pearson')
 
 # Print data skew
 # print(data.skew())
@@ -39,7 +53,7 @@ data_info.correlation_info(data, 'pearson')
 # pyplot.show()
 
 
-
+#Kruskal_wallis
 features_selection.kruskal_wallis(data)
 
 
