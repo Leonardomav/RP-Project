e6b5d016134c272a57c9baf37387c00fce491f22
Leonardomav <leomavieira97@gmail.com>
2019-04-09 18:10:07 +0100

    StandardScaler implemented and some refactor

--- features_selection.py
+++ features_selection.py
@@ -3,8 +3,9 @@ import numpy as np
 
 def kruskal_wallis(data):
     rank=[]
-    for i in range(data.shape[1]-3):
-        setX = data[data.columns[i+2]].tolist()
+    for i in range(data.shape[1]):
+        setX = data[data.columns[i]].tolist()
         setY = data["RainTomorrow"].tolist()
-        rank.append(scipy.stats.kruskal(setX,setY))
+        rank.append((i,scipy.stats.kruskal(setX,setY)))
+    rank = sorted(rank, key=lambda x: x[1]  )
     print(rank)
--- main.py
+++ main.py
@@ -1,62 +1,76 @@
 from matplotlib import pyplot
 import numpy
-from pandas import read_csv, set_option
-from pandas.plotting import scatter_matrix
-from sklearn.preprocessing import MinMaxScaler
+import pandas
+import sklearn.preprocessing
 import scipy
 import data_info
 import features_selection
 
-# Load data set
-filename = 'weatherAUS.csv'
-data_raw = read_csv(filename)
+def categorize_data(data):
+    labels = data['WindGustDir'].astype('category').cat.categories.tolist()
+    col = ['WindGustDir', 'WindDir9am', 'WindDir3pm']
 
-# Remove features that have more than 20% of missing values
-data = data_raw.dropna(1, thresh=len(data_raw.index) * 0.8)
+    for c in col:
+        replace_map = {c: {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
+        data.replace(replace_map, inplace=True)
+        data[c].astype('category')
 
-# Remove examples that have any missing values
-data = data.dropna(0, how='any')
-
-#change Yes and No to 1 and 0
-data['RainTomorrow'] = data['RainTomorrow'].map({'Yes': 1, 'No': 0}).astype('category')
-data['RainToday'] = data['RainToday'].map({'Yes': 1, 'No': 0}).astype('category')
-
-
-labels = data['WindGustDir'].astype('category').cat.categories.tolist()
-col = ['WindGustDir', 'WindDir9am', 'WindDir3pm']
-
-for c in col:
-    replace_map = {c : {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
+    labels = data['Location'].astype('category').cat.categories.tolist()
+    replace_map = {'Location': {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
     data.replace(replace_map, inplace=True)
-    data[c].astype('category')
+    data['Location'].astype('category')
 
-labels = data['Location'].astype('category').cat.categories.tolist()
-replace_map = {'Location': {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
-data.replace(replace_map, inplace=True)
-data['Location'].astype('category')
+    return data
+
+def main():
+    # Load data set
+    filename = 'weatherAUS.csv'
+    data_raw = pandas.read_csv(filename)
+
+    # Remove features that have more than 20% of missing values
+    data = data_raw.dropna(1, thresh=len(data_raw.index) * 0.8)
+
+    # Remove examples that have any missing values
+    data = data.dropna(0, how='any')
+
+    #change Yes and No to 1 and 0
+    data['RainTomorrow'] = data['RainTomorrow'].map({'Yes': 1, 'No': 0}).astype('category')
+    data['RainToday'] = data['RainToday'].map({'Yes': 1, 'No': 0}).astype('category')
+
+    data = categorize_data(data)
+    data_date_loc = data[['Date', 'Location']]
+
+    #normaliza data
+    data_normalize=data.loc[:, ~data.columns.isin(['Date', 'Location'])]
+    x = data_normalize.values
+    scaler=sklearn.preprocessing.StandardScaler()
+    x_scaled=scaler.fit_transform(x)
+    data_normalize = pandas.DataFrame(x_scaled, columns = data_normalize.columns)
+
+    print(data_normalize.head())
+
+    # Print general info about the data
+    #data_info.print_general_information(data)
+
+    # Describe a Single Columns
+    # data_info.describe_column(data)
+
+    # Print correlation between all features or between two if specified
+    #data_info.correlation_info(data, 'pearson')
+
+    # Print data skew
+    # print(data.skew())
+
+    # Plot the features histogram
+    # data.hist()
+    # pyplot.show()
 
 
-# Print general info about the data
-#data_info.print_general_information(data)
-
-# Describe a Single Columns
-# data_info.describe_column(data)
-
-# Print correlation between all features or between two if specified
-#data_info.correlation_info(data, 'pearson')
-
-# Print data skew
-# print(data.skew())
-
-# Plot the features histogram
-# data.hist()
-# pyplot.show()
+    #Kruskal_wallis
+    features_selection.kruskal_wallis(data_normalize)
 
 
-#Kruskal_wallis
-features_selection.kruskal_wallis(data)
-
-
-
+if __name__ == '__main__':
+    main()
 
 
