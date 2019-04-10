LDA e PCA expected to be working | some refactor | normalization fixed

features_selection.py
@@ -1,13 +1,12 @@
 import scipy
-import numpy as np
 
-def kruskal_wallis(data):
+def kruskal_wallis(data, data_y):
     rank=[]
+    setY = data_y['RainTomorrow'].tolist()
     for i in range(data.shape[1]):
         setX = data[data.columns[i]].tolist()
-        setY = data["RainTomorrow"].tolist()
         rank.append((i,scipy.stats.kruskal(setX,setY)[0]))
 
 
-    rank = sorted(rank, key=lambda x: x[1], reverse=True )
+    rank = sorted(rank, key=lambda x: x[1])
     return rank