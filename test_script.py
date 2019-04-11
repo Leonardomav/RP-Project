26881b2c4a06ff4c90d0c749062ed2ab70ead5bf
soren5 <pedrofilipecarvalho@mac.com>
2019-04-11 18:16:41 +0100

    Structure for the test script
    #2

---
+++ test_script.py
@@ -0,0 +1,68 @@
+from test_pipeline import test_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import RFE, SelectKBest, chi2
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import (KFold, LeaveOneOut, ShuffleSplit,
+                                     cross_val_score, train_test_split)
+from sklearn.pipeline import Pipeline
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy
+import pandas
+def categorize_data(data):
+    labels = data['WindGustDir'].astype('category').cat.categories.tolist()
+    col = ['WindGustDir', 'WindDir9am', 'WindDir3pm']
+
+    for c in col:
+        replace_map = {c: {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
+        data.replace(replace_map, inplace=True)
+
+    labels = data['Location'].astype('category').cat.categories.tolist()
+    replace_map = {'Location': {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
+    data.replace(replace_map, inplace=True)
+    data['Location'].astype('category')
+
+    return data
+
+def get_preprocessed_data():
+    # Load data set
+    filename = 'weatherAUS.csv'
+    data_raw = pandas.read_csv(filename)
+
+    # Remove features that have more than 20% of missing values
+    data_less_raw = data_raw.dropna(1, thresh=len(data_raw.index) * 0.8)
+
+    # Remove examples that have any missing values
+    data_less_raw = data_less_raw.dropna(0, how='any')
+
+    # Remove RISK_MM
+    data_less_raw = data_less_raw.drop(['RISK_MM'], axis=1)
+
+    data = data_less_raw.copy()
+    data['RainTomorrow'] = data['RainTomorrow'].map({'Yes': 1, 'No': 0})
+    data['RainToday'] = data['RainToday'].map({'Yes': 1, 'No': 0})
+
+    data = categorize_data(data)
+
+
+    data_date_loc = data[['Date', 'Location']]
+
+    data_y = data['RainTomorrow'].ravel()
+    data = data.drop(['Date', 'Location'], axis=1)
+
+    return {'x': data, 'y': data_y}
+
+data = get_preprocessed_data()
+test_pipeline(
+    data,
+    Pipeline([
+        ('lda', LinearDiscriminantAnalysis())]),
+    0)
