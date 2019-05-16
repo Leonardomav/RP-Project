9ec9b543d676cddbdbb5f3d696d6af5083572c8d
Leonardomav <leomavieira97@gmail.com>
2019-05-16 16:59:19 +0100

    GUI integration WIP

--- graphical_interface.py
+++ graphical_interface.py
@@ -1,7 +1,60 @@
 from tkinter import *
 from tkinter.ttk import *
+import matplotlib
+
+matplotlib.use('TkAgg')
 from test_pipeline import test_pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.pipeline import Pipeline
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.decomposition import PCA
+import pandas
+from feature_selection_pipeline import kruskal_wallis, select_k_best, ROC
+from prediction_pipeline import kfold_cross_val_predictions, train_test_predictions
+
+
+def categorize_data(data):
+    labels = data['WindGustDir'].astype('category').cat.categories.tolist()
+    col = ['WindGustDir', 'WindDir9am', 'WindDir3pm']
+
+    for c in col:
+        replace_map = {c: {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
+        data.replace(replace_map, inplace=True)
+
+    labels = data['Location'].astype('category').cat.categories.tolist()
+    replace_map = {'Location': {k: v for k, v in zip(labels, list(range(1, len(labels) + 1)))}}
+    data.replace(replace_map, inplace=True)
+    data['Location'].astype('category')
+
+    return data
+
+
+def get_preprocessed_data():
+    # Load data set
+    filename = 'weatherAUS.csv'
+    data_raw = pandas.read_csv(filename)
+
+    # Remove features that have more than 20% of missing values
+    data_less_raw = data_raw.dropna(1, thresh=len(data_raw.index) * 0.8)
+
+    # Remove examples that have any missing values
+    data_less_raw = data_less_raw.dropna(0, how='any')
+
+    # Remove RISK_MM
+    data_less_raw = data_less_raw.drop(['RISK_MM'], axis=1)
+    states = ["All"]
+    states.extend(data_less_raw['Location'].unique())
+
+    data = data_less_raw.copy()
+    data['RainTomorrow'] = data['RainTomorrow'].map({'Yes': 1, 'No': 0})
+    data['RainToday'] = data['RainToday'].map({'Yes': 1, 'No': 0})
+
+    data = categorize_data(data)
+
+    data_y = data['RainTomorrow'].ravel()
+    data = data.drop(['Date', 'Location', 'RainTomorrow'], axis=1)
+
+    return {'x': data, 'y': data_y}, states, len(data.columns)
 
 
 def GUI(data, states, n_features, feature_sel, dim_reduction, predict_methods, classifiers):
@@ -93,7 +146,6 @@ def GUI(data, states, n_features, feature_sel, dim_reduction, predict_methods, c
             if comb5.get() == i.__name__:
                 prediction_function = i
 
-
         for i in dim_reduction:
             if comb3.get() == "None":
                 fit_transform_option = None
@@ -110,36 +162,36 @@ def GUI(data, states, n_features, feature_sel, dim_reduction, predict_methods, c
                 classifier = i
                 break
 
-        #try:
-        print("Run config")
-        if comb6.get() == 'mahalanobis' and comb1.get() != "None" and comb3.get() == 'lda-dr':
-            warText.configure(text="This combination is invalid")
-            pass
-        elif comb3.get() != "None":
-            test_pipeline(
-                data,
-                Pipeline([
-                    fit_transform_option,
-                    classifier
-                ]),
-                seed,
-                feature_selection_function=feature_selection_function,
-                prediction_function=prediction_function)
-        else:
-            test_pipeline(
-                data,
-                Pipeline([
-                    classifier
-                ]),
-                seed,
-                feature_selection_function=feature_selection_function,
-                prediction_function=prediction_function)
-        warText.configure(text="")
-        print("passou")
-        #except:
+        try:
+            print("Run config")
+            if comb6.get() == 'mahalanobis' and comb1.get() != "None" and comb3.get() == 'lda-dr':
+                warText.configure(text="This combination is invalid")
 
-         #   print("porque")
-          #  warText.configure(text="This combination is invalid")
+            elif comb3.get() != "None":
+                test_pipeline(
+                    data,
+                    Pipeline([
+                        fit_transform_option,
+                        classifier
+                    ]),
+                    seed,
+                    feature_selection_function=feature_selection_function,
+                    prediction_function=prediction_function)
+                warText.configure(text="")
+
+            else:
+                test_pipeline(
+                    data,
+                    Pipeline([
+                        classifier
+                    ]),
+                    seed,
+                    feature_selection_function=feature_selection_function,
+                    prediction_function=prediction_function)
+                warText.configure(text="")
+        except Exception as e:
+            print(e)
+            warText.configure(text="This combination is invalid")
 
     btn = Button(window, text="Run Configuration", command=clicked)
 
@@ -147,3 +199,41 @@ def GUI(data, states, n_features, feature_sel, dim_reduction, predict_methods, c
 
     window.geometry('655x275')
     window.mainloop()
+
+
+data, states, num_columns = get_preprocessed_data()
+
+fit_transform_options = [
+    None,
+    ('lda-dr', LinearDiscriminantAnalysis()),
+    ('pca', PCA()),
+]
+
+classifiers = [
+    ('lda', LinearDiscriminantAnalysis()),
+    ('euclidean', NearestCentroid(metric='euclidean')),
+    ('mahalanobis', NearestCentroid(metric='mahalanobis')),
+]
+
+feature_selection_functions = [
+    None,
+    kruskal_wallis,
+    select_k_best,
+    ROC,
+]
+
+prediction_functions = [
+    kfold_cross_val_predictions,
+    train_test_predictions,
+]
+
+selected_features = [
+    3,
+    5,
+    10,
+]
+
+seeds_to_test = 1
+
+GUI(data, states, num_columns, feature_selection_functions, fit_transform_options, prediction_functions,
+    classifiers)
