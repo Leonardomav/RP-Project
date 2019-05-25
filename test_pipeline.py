3c976b5103984aa8a33f6facbda1519af7f6ac88
soren5 <pedrofilipecarvalho@mac.com>
2019-05-25 22:32:42 +0100

    hybrid and results

--- test_pipeline.py
+++ test_pipeline.py
@@ -6,6 +6,7 @@ matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from sklearn.metrics import (accuracy_score, classification_report, confusion_matrix)
 from visualizations import plot_confusion_matrix
+from prediction_pipeline import kfold_cross_val_predictions
 
 
 def test_pipeline(data, pipeline, seed, n_features=16, feature_selection_function=None, prediction_function=None,
@@ -114,7 +115,7 @@ def test_pipeline_state(data, data_state, pipeline, seed, n_features=16, feature
         )
         writer.writerow(csv_values)
 
-    tested_data, predictions,results = prediction_function(pipeline, data_state, data_state, seed)
+    tested_data, predictions,results = kfold_cross_val_predictions(pipeline, data_state, seed)
     conf_matrix = confusion_matrix(tested_data, predictions)
     class_report = classification_report(tested_data, predictions, output_dict=True)
     if visual:
@@ -138,3 +139,60 @@ def test_pipeline_state(data, data_state, pipeline, seed, n_features=16, feature
     
     print('Done')
     return results
+
+
+def test_pipeline_hybrid(data, pipeline, seed, n_features=16, feature_selection_function=None, prediction_function=None,
+                  visual=False, region='All'):
+    csv_values = [str(datetime.datetime.now())]
+    procedure_name_list = ''
+    for procedure in pipeline.named_steps:
+        procedure_name_list += procedure + ';'
+    csv_values.append(procedure_name_list)
+    csv_values.append(str(seed))
+    if feature_selection_function is not None:
+        csv_values.append(feature_selection_function.__name__)
+    else:
+        csv_values.append("None")
+    if prediction_function is not None:
+        csv_values.append(prediction_function.__name__)
+    else:
+        csv_values.append("None")
+    if feature_selection_function is not None:
+        for i in data:
+            foo = feature_selection_function(i, n_features, seed)
+            aux = i['x'][foo]
+            i['x'] = aux
+    tested_data = []
+    predictions = []
+    results = []
+    for i in data:
+        tested_data_i, predictions_i, results_i = prediction_function(pipeline, i, seed)
+        tested_data.extend(tested_data_i)
+        predictions.extend(predictions_i)
+        results.extend(results_i)
+    csv_values.append(str(n_features))
+    conf_matrix = confusion_matrix(tested_data, predictions)
+    class_report = classification_report(tested_data, predictions, output_dict=True)
+    csv_values.append(region)
+    if visual:
+        plot_confusion_matrix(tested_data, predictions, [0, 1],
+                              normalize=False,
+                              title=None,
+                              cmap=plt.cm.Blues)
+        plt.show()
+    with open('results_hybrid.csv', 'a') as result_csv:
+        writer = csv.writer(result_csv)
+        csv_values.extend([
+            class_report['0']['f1-score'],
+            class_report['0']['precision'],
+            class_report['0']['recall'],
+            class_report['1']['f1-score'],
+            class_report['1']['precision'],
+            class_report['1']['recall'],
+            accuracy_score(tested_data, predictions)]
+        )
+        writer.writerow(csv_values)
+
+    print('Done')
+    return results
+
---
+++ test_script_hybrid.py
@@ -0,0 +1,135 @@
+import copy
+
+import matplotlib
+import pandas
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors.kde import KernelDensity
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+import data_preprocessment
+from feature_selection_pipeline import ROC, kruskal_wallis, select_k_best, kernel_density_fs, RFE_fs
+from prediction_pipeline import kfold_cross_val_predictions, train_test_predictions
+from test_pipeline import test_pipeline, test_pipeline_hybrid
+import visualizations
+
+matplotlib.use('TkAgg')
+
+data, states, num_columns, data_loc, state_dict = data_preprocessment.get_preprocessed_data()
+
+
+# NSW - New South Wales
+# VIC - Victoria
+# QNL - Queensland
+# SAU - South Australia
+# TAS - Tasmania
+# WAU - Western Australia
+# NRT - Northen Territoty
+
+states = [
+    #"All",
+    "NSW",
+    "VIC",
+    "QNL",
+    "SAU",
+    "TAS",
+    "WAU",
+    "NRT",
+]
+
+#if "All" not in states:
+#    data = data_preprocessment.select_location(data_loc, states, state_dict)
+
+fit_transform_options = [
+    #None,
+    ('lda-dr', LinearDiscriminantAnalysis()),
+    ('pca', PCA()),
+]
+
+classifiers = [
+    #('lda', LinearDiscriminantAnalysis()),
+     ('euclidean', NearestCentroid(metric='euclidean')),
+     ('mahalanobis', NearestCentroid(metric='mahalanobis')),
+    #('bayes', GaussianNB()),
+    #('knearest_neighbours', KNeighborsClassifier()),
+    #('svm', SVC()),
+]
+
+feature_selection_functions = [
+    None,
+    # kruskal_wallis,
+    #select_k_best,
+    # kernel_density_fs
+    # ROC,
+    # RFE_fs,
+]
+
+prediction_functions = [
+    kfold_cross_val_predictions,
+    # train_test_predictions,
+]
+
+selected_features = [
+    # 3,
+    5,
+    # 10,
+    # 16,
+]
+
+seeds_to_test = 1
+
+hybrid_data = []
+for region in states:
+    aux = data_preprocessment.select_location(copy.deepcopy(data_loc), [region], state_dict)
+    hybrid_data.append(aux)
+predictions_list = []
+predictions_list.append(test_pipeline_hybrid(
+    copy.deepcopy(hybrid_data),
+    Pipeline([
+    ('lda-dr', LinearDiscriminantAnalysis()),
+    ('euclidean', NearestCentroid(metric='euclidean')),
+    ]),
+    1,
+    n_features=5,
+    feature_selection_function=None,
+    prediction_function=kfold_cross_val_predictions,
+    region=region) )
+
+predictions_list.append(test_pipeline_hybrid(
+    copy.deepcopy(hybrid_data),
+    Pipeline([
+    ('lda-dr', LinearDiscriminantAnalysis()),
+    ('lda', LinearDiscriminantAnalysis()),
+    ]),
+    1,
+    n_features=5,
+    feature_selection_function=None,
+    prediction_function=kfold_cross_val_predictions,
+    region=region) )
+
+predictions_list.append(test_pipeline_hybrid(
+    copy.deepcopy(hybrid_data),
+    Pipeline([
+    ('lda', LinearDiscriminantAnalysis()),
+    ]),
+    1,
+    n_features=5,
+    feature_selection_function=RFE_fs,
+    prediction_function=kfold_cross_val_predictions,
+    region=region) )
+
+predictions_list.append(test_pipeline_hybrid(
+    copy.deepcopy(hybrid_data),
+    Pipeline([
+    ('mahalanobis', NearestCentroid(metric='mahalanobis')),
+    ]),
+    1,
+    n_features=5,
+    feature_selection_function=RFE_fs,
+    prediction_function=kfold_cross_val_predictions,
+    region=region) )
+
+#visualizations.box_plot_comparison(predictions_list, ["lda-dr euclidean", "lda-dr lda", "rfe lda", "rfe mahalanobis"])
