069bb224e55f485a12fdcd6ae8bf68abd779ed0a
soren5 <pedrofilipecarvalho@mac.com>
2019-05-25 12:58:47 +0100

    Pqp so quero das push

--- test_script.py
+++ test_script.py
@@ -20,27 +20,27 @@ matplotlib.use('TkAgg')
 data, states, num_columns, data_loc = data_preprocessment.get_preprocessed_data()
 
 fit_transform_options = [
-    None,
-    #('lda-dr', LinearDiscriminantAnalysis()),
-    #('pca', PCA()),
+    #None,
+    ('lda-dr', LinearDiscriminantAnalysis()),
+    ('pca', PCA()),
     #('kernel_density', KernelDensity(kernel='gaussian')),
     #('knearest_neighbours', KNeighborsClassifier()),
 ]
 
 classifiers = [
     ('lda', LinearDiscriminantAnalysis()),
-    ('euclidean', NearestCentroid(metric='euclidean')),
-    ('mahalanobis', NearestCentroid(metric='mahalanobis')),
+    #('euclidean', NearestCentroid(metric='euclidean')),
+    #('mahalanobis', NearestCentroid(metric='mahalanobis')),
     ('bayes', GaussianNB()),
     ('knearest_neighbours', KNeighborsClassifier()),
     ('svm', SVC()),
 ]
 
 feature_selection_functions = [
-    #None,
-    kruskal_wallis,
-    select_k_best,
-    ROC,
+    None,
+    #kruskal_wallis,
+    #select_k_best,
+    #ROC,
 ]
 
 prediction_functions = [
@@ -55,35 +55,35 @@ selected_features = [
     #16,
 ]
 
-seeds_to_test = 1
+seeds_to_test = 5
 
-for fit_transform_option in fit_transform_options:
-    for classifier in classifiers:
-        for feature_selection_function in feature_selection_functions:
-            for prediction_function in prediction_functions:
-                for n_feature in selected_features:
-                    for seed in range(seeds_to_test):
-                        if classifier[0] == 'mahalanobis' and fit_transform_option is not None and fit_transform_option[
-                            0] == 'lda-dr':
-                            pass
-                        elif fit_transform_option is not None:
-                            test_pipeline(
-                                copy.deepcopy(data),
-                                Pipeline([
-                                    fit_transform_option,
-                                    classifier
-                                ]),
-                                seed,
-                                n_features=n_feature,
-                                feature_selection_function=feature_selection_function,
-                                prediction_function=prediction_function)
-                        else:
-                            test_pipeline(
-                                copy.deepcopy(data),
-                                Pipeline([
-                                    classifier
-                                ]),
-                                seed,
-                                n_features=n_feature,
-                                feature_selection_function=feature_selection_function,
-                                prediction_function=prediction_function)
+for seed in range(seeds_to_test):
+    for fit_transform_option in fit_transform_options:
+        for classifier in classifiers:
+            for feature_selection_function in feature_selection_functions:
+                for prediction_function in prediction_functions:
+                    for n_feature in selected_features:
+                            if classifier[0] == 'mahalanobis' and fit_transform_option is not None and fit_transform_option[
+                                0] == 'lda-dr':
+                                pass
+                            elif fit_transform_option is not None:
+                                test_pipeline(
+                                    copy.deepcopy(data),
+                                    Pipeline([
+                                        fit_transform_option,
+                                        classifier
+                                    ]),
+                                    seed,
+                                    n_features=n_feature,
+                                    feature_selection_function=feature_selection_function,
+                                    prediction_function=prediction_function)
+                            else:
+                                test_pipeline(
+                                    copy.deepcopy(data),
+                                    Pipeline([
+                                        classifier
+                                    ]),
+                                    seed,
+                                    n_features=n_feature,
+                                    feature_selection_function=feature_selection_function,
+                                    prediction_function=prediction_function)
